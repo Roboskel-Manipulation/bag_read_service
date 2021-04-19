2afc95016f37cd25d746537dfc6834280ee951bd
fotinidelig <fotinidelig@gmail.com>
2021-04-19 12:00:47 +0300

    autonomous analysis pipeline

--- src/clientBag.py
+++ src/clientBag.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 import sys
-import rospy
+import rospy, rosbag, time
 from std_srvs.srv import Empty, Trigger, EmptyRequest
+from openpose_ros_msgs.msg import OpenPoseHumanList
 from keypoint_3d_matching_msgs.msg import Keypoint3d_list
-
+from std_msgs.msg import Bool
 
 # 3D topic name: /keypoint_3d_matching
 
-def callback(Keypoints_v):
+def openpose_callback(msg):
+	global bag_write_openpose
+	bag_write_openpose.write("/openpose_ros/human_list", msg)
+
+def keypoints3d_callback(msg):
+	global bag_write_3D
+	bag_write_3D.write("/transform_topic", msg)
+
+def callback(msg):
+	global count, listen_topic
+	count += 1
 	rospy.wait_for_service('/next_msg')
 	try:
 		# Create an empty request
@@ -19,13 +30,52 @@ def callback(Keypoints_v):
 	except rospy.ServiceException, e:
 		print "Service call failed: %s"%e
 
+def end_rosbag_read_callback(msg):
+	global end_flag
+	end_flag = True
 
 if __name__ == "__main__":
-	rospy.init_node('clientBag', anonymous=True)
+	end_flag = False
+	count = 0
+	rospy.init_node('clientBag')
+	fileName = sys.argv[1].split('/')[-1].split('.')[0]
+
+	bag_write_3D = rosbag.Bag('/home/fdeli/test_autonomous_bag_read_server/rosbags/3D_new/CXF/{}.bag'.format(fileName), 'w')
+	bag_write_openpose = rosbag.Bag('/home/fdeli/test_autonomous_bag_read_server/rosbags/2D/CXF/{}.bag'.format(fileName), 'w')
+
+	bag = rosbag.Bag(sys.argv[1], 'r')
+	info = bag.get_type_and_topic_info()
+	topics = list(info[1].keys())
+	messages = {}
+	for topic in topics:
+		messages[topic] = bag.read_messages(topics=topic)
+	max_length = min([sum([1 for _ in messages[i]]) for i in messages.keys()])
 
 	# Create the connection to the service.
 	service = rospy.ServiceProxy('/next_msg', Empty)
 
 	listen_topic = rospy.get_param('~listen_topic') 
+	
+	# subscriber to keypoint_3d_matching (3D keypoints expressed in the camera frame)
 	topic_sub = rospy.Subscriber(listen_topic, Keypoint3d_list, callback)
-	rospy.spin()
+	
+	# subscriber to openpose_ros/human list (openpose keypoints)
+	openpose_sub = rospy.Subscriber("/openpose_ros/human_list", OpenPoseHumanList, openpose_callback)
+
+	# subscriber to transform_topic (3D keypoints expressed in the base_link frame)
+	keypoints3d_sub = rospy.Subscriber("/transform_topic", Keypoint3d_list, keypoints3d_callback)
+
+	# subscriber to the end_rosbag_read (topic is published when the rosbag reader reads the entire rosbag)
+	end_rosbag_read_sub = rospy.Subscriber("/end_rosbag_read", Bool, end_rosbag_read_callback)
+
+	while True:
+		print(count, max_length)
+		if end_flag:
+			break
+
+	rospy.loginfo("Gonna sleep for 2 secs")
+	time.sleep(2)
+	rospy.loginfo("Woke up....gonna exit")
+	bag.close()
+	bag_write_3D.close()
+	bag_write_openpose.close()
--- src/readBagService.py
+++ src/readBagService.py
@@ -8,10 +8,11 @@ the Server reads the next message for each topic in the rosbag file and publishe
 
 from sensor_msgs.msg import Image, PointCloud2, CameraInfo
 import roslib, rospy, rosbag
-from std_srvs.srv import Empty,EmptyResponse, Trigger
-import sys
+from std_srvs.srv import Empty, EmptyRequest, EmptyResponse, Trigger
+import sys, time
 from rosgraph_msgs.msg import Clock, Log
 from tf2_msgs.msg import TFMessage
+from std_msgs.msg import Bool
 from keypoint_3d_matching_msgs.msg import Keypoint3d_list, Keypoint3d
 
 __author__ = "Lygerakis Fotios"
@@ -27,7 +28,8 @@ class ReadRosBagService():
         self.messages = {}
         self.listOfCounts = {}
         self.listOfEndConditions = {}
-        
+        self.count = 0
+
         #Create a publisher for each topic
         self.topics, self.msg_types = self.get_topic_and_type_list()
 
@@ -48,6 +50,24 @@ class ReadRosBagService():
         self.clockPublisher = rospy.Publisher("/clock", Clock, queue_size = 10)
         # create a Server
         self.next_msgs_srv = rospy.Service('/next_msg', Empty, self.service)
+
+        # sometimes, more than one service call was needed for the pipeline to start
+        # the count "7" was experimentally chosen
+        while self.count < 7:
+            rospy.loginfo("Gonna sleep for 3 secs")
+            time.sleep(3)
+            rospy.loginfo("Woke up")
+            service = rospy.ServiceProxy('/next_msg', Empty)
+            rospy.wait_for_service('/next_msg')
+            try:
+                # Create an empty request
+                req = EmptyRequest()
+                # use the service
+                service(req)
+            except rospy.ServiceException, e:
+                print "Service call failed: %s"%e
+            self.count += 1
+
 
     def get_topic_and_type_list(self):
         # retrieve a list of topics from the rosbag file
@@ -90,7 +110,9 @@ if __name__=='__main__':
     
     # Rosbag file 
     bag_file = sys.argv[1]
-    
+
+    end_rosbag_read_pub = rospy.Publisher("/end_rosbag_read", Bool, queue_size=10)
+   
     #Run this file with the name of the bag file
     rrbs = ReadRosBagService(bag_file)
 
@@ -103,5 +125,9 @@ if __name__=='__main__':
         print("Bag Reader: Total published msgs from topic %s: %d "%(topic, rrbs.listOfCounts[topic]))
     
     # Wait a bit before closing the rosbag
-    rospy.sleep(0.5)
+    time.sleep(0.5)
+    rospy.loginfo("Gonna close input rosbag")
     rrbs.bag.close()
+    end_msg = Bool()
+    end_msg.data = True
+    end_rosbag_read_pub.publish(end_msg)
